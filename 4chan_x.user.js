8237adb2acbe9c4882d86e209a70bd20fe1a005c
Nicolas Stepien <stepien.nicolas@gmail.com>
2012-12-19 13:54:37 +0100

    Opera is getting better. Slowly.

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -2314,27 +2314,7 @@
       return QR.resetFileInput();
     },
     resetFileInput: function() {
-      var clone, input;
-      input = $('[type=file]', QR.el);
-      input.value = null;
-      if ($.engine !== 'presto') {
-        return;
-      }
-      clone = $.el('input', {
-        type: 'file',
-        accept: input.accept,
-        max: input.max,
-        multiple: input.multiple,
-        size: input.size,
-        title: input.title
-      });
-      $.on(clone, 'change', QR.fileInput);
-      $.on(clone, 'click', function(e) {
-        if (e.shiftKey) {
-          return QR.selected.rmFile() || e.preventDefault();
-        }
-      });
-      return $.replace(input, clone);
+      return $('[type=file]', QR.el).value = null;
     },
     replies: [],
     reply: (function() {
@@ -2399,10 +2379,10 @@
           this.el.style.backgroundImage = null;
           return;
         }
-        url = window.URL || window.webkitURL;
-        if (typeof url.revokeObjectURL === "function") {
-          url.revokeObjectURL(this.url);
+        if (!(url = window.URL || window.webkitURL)) {
+          return;
         }
+        url.revokeObjectURL(this.url);
         fileUrl = url.createObjectURL(file);
         img = $.el('img');
         $.on(img, 'load', function() {
