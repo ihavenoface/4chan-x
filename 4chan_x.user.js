16e9d3d11fa260d094659eccaaae59d269ffb2fd
noface <noface@outlook.com>
2013-01-18 23:31:40 +0100

    Release 2.37.9.

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           4chan x
-// @version        2.37.8
+// @version        2.37.9
 // @namespace      aeosynth
 // @description    Adds various features.
 // @copyright      2009-2011 James Campos <james.r.campos@gmail.com>
@@ -27,7 +27,7 @@
  * Copyright (c) 2009-2011 James Campos <james.r.campos@gmail.com>
  * Copyright (c) 2012 Nicolas Stepien <stepien.nicolas@gmail.com>
  * http://mayhemydg.github.com/4chan-x/
- * 4chan X 2.37.8
+ * 4chan X 2.37.9
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -6746,7 +6746,7 @@
       }
     },
     namespace: '4chan_x.',
-    version: '2.37.8',
+    version: '2.37.9',
     callbacks: [],
     css: '\
 /* dialog styling */\
--- Cakefile
+++ Cakefile
@@ -2,7 +2,7 @@
 {exec} = require 'child_process'
 fs     = require 'fs'
 
-VERSION = '2.37.8'
+VERSION = '2.37.9'
 
 HEADER  = """
 // ==UserScript==
