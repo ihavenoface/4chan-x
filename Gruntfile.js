ce43d920306ce973e9c6595232f1cf3abfc8fac9
Nicolas Stepien <stepien.nicolas@gmail.com>
2013-04-27 04:30:31 +0200

    Move some html out the code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,6 +121,7 @@ module.exports = function(grunt) {
           'package.json',
           'lib/**/*',
           'src/**/*',
+          'html/**/*',
           'css/**/*',
           'img/**/*'
         ],
