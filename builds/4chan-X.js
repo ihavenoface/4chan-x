2c0c3e6129f5d6aee0e870e964c33691e8f0b95e
noface <noface@outlook.com>
2013-05-21 23:12:06 +0200

    More fixes.

--- builds/4chan-X.js
+++ builds/4chan-X.js
@@ -6908,7 +6908,7 @@
       if (g.VIEW === 'catalog' || !Conf['Linkification']) {
         return;
       }
-      this.catchAll = /([a-z]+:(?:\/\/)?[^\s\/$\.\#><].|([0-9a-zA-Z-\.]+(@|\.(?:a(?:e(?:ro)?|s(?:ia)?|r(?:pa)?|[cdfgilmnoqtuwxz])|b(?:iz?|[abdefghjmnorstvwyz])|c(?:at?|o(?:(?:op|m))?|[cdfghiklmnruvxyz])|e(?:du|[cegrstu])|g(?:ov|[abdefghilmnpqrstuwy])|i(?:n(?:(?:fo|t))?|[delmoqrst])|j(?:o(?:bs)?|[emp])|m(?:il|o(?:bi)?|u(?:seum)?|[acdeghklnprstvwxyz])|n(?:a(?:me)?|et?|om?|[cfgilpruz])|org|p(?:ro?|[aefghkmnstwy])|t(?:el|r(?:avel)?|[cdfghjklmnoptvwz])|d[ejkmoz]|f[ijkmor]|h[kmnrtu]|k[eghimnprwyz]|l[abcikrstuvy]|qa|r[easuw]|s[abcdegijklmnortuvyz]|u[agksyz]|v[aceginu]|w[fs]|y[etu]|z[amw])(?![\d\w]))))[^\s><]*/i;
+      this.catchAll = /([a-z]*:(?:\/\/|\?)[^\s\/$\.\\\#><].|(\w[\w-\.]*(@[\w-\.]*)?\.(?:a(?:e(?:ro)?|s(?:ia)?|r(?:pa)?|[cdfgilmnoqtuwxz])|b(?:iz?|[abdefghjmnorstvwyz])|c(?:at?|o(?:(?:op|m))?|[cdfghiklmnruvxyz])|e(?:du|[cegrstu])|g(?:ov|[abdefghilmnpqrstuwy])|i(?:n(?:(?:fo|t))?|[delmoqrst])|j(?:o(?:bs)?|[emp])|m(?:il|o(?:bi)?|u(?:seum)?|[acdeghklnprstvwxyz])|n(?:a(?:me)?|et?|om?|[cfgilpruz])|org|p(?:ro?|[aefghkmnstwy])|t(?:el|r(?:avel)?|[cdfghjklmnoptvwz])|d[ejkmoz]|f[ijkmor]|h[kmnrtu]|k[eghimnprwyz]|l[abcikrstuvy]|qa|r[easuw]|s[abcdegijklmnortuvyz]|u[agksyz]|v[aceginu]|w[fs]|y[etu]|z[amw])(?![\d\w])))[^\s><]*/i;
       this.protocol = /((?:http(?::\/\/|s:\/\/)|ftp(?::\/\/|s:\/\/)|ma(?:gnet:\??|ilto:)|irc:(?:\/\/)?|r(?:mtp:\/\/|tmp(?:t:\/\/|s:\/\/))|byond:\/\/)[^\s\/$.?\#].|www\.)[^\s><]*/i;
       this.globalCatchAll = new RegExp(this.catchAll.source, 'g');
       return Post.prototype.callbacks.push({
@@ -6932,6 +6932,8 @@
         }
         if (Linkify.matchingProtocol = Linkify.protocol.exec(link)) {
           link = Linkify.matchingProtocol[0];
+        } else if (/\.{2}|-{2}/.test(link)) {
+          continue;
         }
         href = !/^www/.test(link) && Linkify.matchingProtocol || /^\w+:\/\//.test(link) ? link : /@/.test(link) ? "mailto:" + link : "http://" + link;
         Linkify.href = href.replace(/\\/g, '\/');
